1babe4e5eda2ad68fc08ff2a057e9e4b4f78ff74
fangsfmac <highestfeng@gmail.com>
2018-04-23 14:19:29 +0800

    自定义圆形进度条view

--- app/src/main/java/com/example/fangsfmac/customview/MainActivity.java
+++ app/src/main/java/com/example/fangsfmac/customview/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fangsfmac.customview;
 import android.view.View;
 
 import com.example.fangsfmac.customview.colorTrackTextView_02.ColorTrackTextViewActivity;
+import com.example.fangsfmac.customview.loadingView_03.LoadingViewActivity;
 import com.example.fangsfmac.customview.progressLoading_01.ProgressLoadingActivity;
 
 public class MainActivity extends BaseActivity {
@@ -23,5 +24,9 @@ public class MainActivity extends BaseActivity {
 
     public void click2(View view) {
         jumpAct(ColorTrackTextViewActivity.class);
+    }
+
+    public void click3(View view) {
+        jumpAct(LoadingViewActivity.class);
     }
 }
--- app/src/main/java/com/example/fangsfmac/customview/loadingView_03/LoadingViewActivity.java
+++ app/src/main/java/com/example/fangsfmac/customview/loadingView_03/LoadingViewActivity.java
@@ -1,9 +1,8 @@
-package com.example.fangsfmac.customview.progressLoading_01;
+package com.example.fangsfmac.customview.loadingView_03;
 
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 
@@ -13,40 +12,37 @@ import com.example.fangsfmac.customview.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ProgressLoadingActivity extends BaseActivity {
+public class LoadingViewActivity extends BaseActivity {
+
 
     @BindView(R.id.progressView)
-    ProgressLoadView mProgressView;
+    ProgressView mProgressView;
     private ValueAnimator mValueAnimator;
 
     @Override
     protected int bindLayout() {
-        return R.layout.activity_progress_loading;
+        return R.layout.activity_loading_view;
     }
 
     @Override
     protected void init() {
-        mProgressView.setMaxProgress(4000);
-        mValueAnimator = ObjectAnimator.ofFloat(0, 3500);
-        mValueAnimator.setDuration(1000);
+        mProgressView.setMaxProgress(100);
+        mValueAnimator = ObjectAnimator.ofFloat(1, 80);
+
         mValueAnimator.setInterpolator(new DecelerateInterpolator());
         mValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                float currenStep = (float) animation.getAnimatedValue();
-                mProgressView.setCurrentProgress((int) currenStep);
+
+                mProgressView.setCurrentProgress((Float) animation.getAnimatedValue());
+
             }
         });
+        mValueAnimator.setDuration(1500);
         mValueAnimator.start();
     }
 
     public void click1(View view) {
         mValueAnimator.start();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mValueAnimator.cancel();
     }
 }
