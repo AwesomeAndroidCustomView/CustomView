8a99b7e816c328bb1ab68be02854b38c0ac9f41a
fangsfmac <highestfeng@gmail.com>
2018-05-01 16:24:15 +0800

    自定义字母索引

--- app/src/main/java/com/example/fangsfmac/customview/letterIndexView_06/LetterIndexActivity.java
+++ app/src/main/java/com/example/fangsfmac/customview/letterIndexView_06/LetterIndexActivity.java
@@ -1,13 +1,23 @@
 package com.example.fangsfmac.customview.letterIndexView_06;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.fangsfmac.customview.BaseActivity;
 import com.example.fangsfmac.customview.R;
 
-public class LetterIndexActivity extends BaseActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class LetterIndexActivity extends BaseActivity implements LetterIndexView.SideBarTouchListener {
+
+
+    @BindView(R.id.tvLetter)
+    TextView mTvLetter;
+    @BindView(R.id.letterIndex)
+    LetterIndexView mLetterIndexView;
 
     @Override
     protected int bindLayout() {
@@ -17,5 +27,25 @@ public class LetterIndexActivity extends BaseActivity {
     @Override
     protected void init() {
 
+        mLetterIndexView.setOnSideTouchListener(this);
+
+    }
+
+    @Override
+    public void onTouch(final String letter, boolean isTouch) {
+        if (isTouch) {
+            mTvLetter.setVisibility(View.VISIBLE);
+            mTvLetter.setText(letter);
+        } else {
+            // 延迟消失
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mTvLetter.setVisibility(View.GONE);
+                    mTvLetter.setText(letter);
+                }
+            }, 180);
+
+        }
     }
 }
--- app/src/main/java/com/example/fangsfmac/customview/letterIndexView_06/LetterIndexView.java
+++ app/src/main/java/com/example/fangsfmac/customview/letterIndexView_06/LetterIndexView.java
@@ -2,11 +2,13 @@ package com.example.fangsfmac.customview.letterIndexView_06;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.example.fangsfmac.customview.R;
@@ -29,6 +31,9 @@ public class LetterIndexView extends View {
     private int mSelectedColor = Color.BLUE;
 
     private int mLetterTextSize = 12;
+    private int mLetterWidth;
+    private int mLetterHeight;
+    private String mCurrentSelectedLetter = "";
 
     public LetterIndexView(Context context) {
         this(context, null);
@@ -70,7 +75,102 @@ public class LetterIndexView extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
+        // 宽度 = paddingleft + paddingRight + 一个字母的宽度
+        mLetterWidth = (int) mDefaultPaint.measureText("A");
 
+        int width = mLetterWidth + getPaddingLeft() + getPaddingRight();
+
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+
+        setMeasuredDimension(width, height);
 
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        //一个字母所占的高度
+        mLetterHeight = getHeight() / mLetters.length;
+
+        for (int i = 0; i < mLetters.length; i++) {
+
+            // 每个 字母的width 可能会不一样, 需要重新计算
+            int letterX = (int) (getWidth() / 2 - mDefaultPaint.measureText(mLetters[i]) / 2);
+
+            Paint.FontMetrics fontMetrics = mDefaultPaint.getFontMetrics();
+            int dy = (int) (((fontMetrics.bottom - fontMetrics.bottom) / 2) - fontMetrics.bottom);
+
+            // 每个字母当前位置的 所在的高度
+            int itemHeight = mLetterHeight + (mLetterHeight * i);
+            int baseLine = itemHeight + dy;
+
+            if (mCurrentSelectedLetter.equals(mLetters[i])) {
+                // 高亮
+                canvas.drawText(mLetters[i], letterX, baseLine, mSelectedPaint);
+            } else {
+                // 动态改变,x,y 的位置
+                canvas.drawText(mLetters[i], letterX, baseLine, mDefaultPaint);
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            //case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+
+                // 判断所选中的位置, 当前位置高亮显示
+                float moveY = event.getY();
+
+                //当前选中 字母的位置
+                int touchIndex = (int) (moveY / mLetterHeight);
+
+
+                if (mSideBarTouchListener != null) {
+                    mSideBarTouchListener.onTouch(mCurrentSelectedLetter, true);
+                }
+
+                if (touchIndex < 0) {
+                    touchIndex = 0;
+                }
+
+                if (touchIndex > mLetters.length - 1) {
+                    touchIndex = mLetters.length - 1;
+                }
+
+                if (mCurrentSelectedLetter.equals(mLetters[touchIndex])) {
+                    return true;
+                }
+
+                mCurrentSelectedLetter = mLetters[touchIndex];
+                invalidate();  // 注: 需要减少 onDraw() 的次数
+
+                break;
+
+            case MotionEvent.ACTION_UP:
+                if (mSideBarTouchListener != null) {
+                    mSideBarTouchListener.onTouch(mCurrentSelectedLetter, false);
+                }
+
+                break;
+
+        }
+
+        return true;
+    }
+
+    private SideBarTouchListener mSideBarTouchListener;
+
+    public void setOnSideTouchListener(SideBarTouchListener listener) {
+        this.mSideBarTouchListener = listener;
+    }
+
+    public interface SideBarTouchListener {
+        // 当前的文字, 文字是否按下了
+        void onTouch(String letter, boolean isTouch);
+    }
+
 }
